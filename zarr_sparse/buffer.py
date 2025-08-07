580f20b85d7acba32a24cac93990a890eafd5dcc
Justus Magin <keewis@posteo.de>
2025-08-07 12:10:22 +0200

    rewrite `all_equal` and `__getitem__`

--- zarr_sparse/buffer.py
+++ zarr_sparse/buffer.py
@@ -78,6 +78,63 @@ def decompose_by_chunks(indexer, chunks):
         return _decompose_array_by_chunks(indexer, offsets, chunksizes)
 
 
+def expand_chunks(chunks, shape):
+    def _expand(chunkspec, size):
+        if chunkspec == -1:
+            return (size,)
+        elif isinstance(chunkspec, int):
+            n_full_chunks, remainder = divmod(size, chunkspec)
+            chunks = (chunkspec,) * n_full_chunks
+
+            if remainder > 0:
+                chunks += (remainder,)
+
+            return chunks
+
+        chunkspec = tuple(chunkspec)
+        if sum(chunkspec) != size:
+            raise ValueError(f"chunks don't add up to the full size: {chunkspec}")
+
+        return chunkspec
+
+    return tuple(_expand(chunkspec, size) for chunkspec, size in zip(chunks, shape))
+
+
+def slice_to_chunk_index(slice_, offsets):
+    if slice_ == slice(None):
+        return 0
+
+    chunk_index = np.flatnonzero(offsets == slice_.start)
+
+    if chunk_index.size == 0:
+        raise ValueError(f"Selected chunk out of bounds: {slice_}")
+
+    return chunk_index[0]
+
+
+def sparse_equal(a, b, equal_nan: bool) -> bool:
+    equal_nan = equal_nan if a.dtype.kind not in ("U", "S", "T", "O", "V") else False
+
+    if b.ndim == 0:
+        return np.array_equal(
+            a.fill_value, getattr(b, "fill_value", b), equal_nan=equal_nan
+        ) and (
+            a.nnz == 0
+            or np.equal(
+                a.data,
+                np.broadcast_to(b.data, a.data.shape),
+                equal_nan,
+            )
+        )
+
+    # use array_equal to obtain equal_nan=True functionality
+    # Since fill-value is a scalar, isn't there a faster path than allocating a new array for fill value
+    # every single time we have to write data?
+    _data, other = sparse.broadcast_arrays(a, b)
+
+    return sparse.equal(a, other, equal_nan=equal_nan)
+
+
 class ChunkGrid:
     """Chunk grid that records slice assignment"""
 
@@ -89,15 +146,21 @@ class ChunkGrid:
 
         if chunks is None:
             chunks = shape
-        self._chunks = chunks
 
-        grid_shape = tuple(math.ceil(s / c) for s, c in zip(shape, chunks))
+        self._chunks = expand_chunks(chunks, shape)
+        self._offsets = tuple(
+            np.cumulative_sum(c, include_initial=True)[:-1] for c in self._chunks
+        )
+        grid_shape = tuple(math.ceil(s / max(c)) for s, c in zip(shape, self._chunks))
         self._data = np.full(grid_shape, dtype=object, fill_value=None)
-        self._slices = np.full(grid_shape, dtype=object, fill_value=None)
 
     @property
     def shape(self):
         return self._shape
+
+    @property
+    def ndim(self):
+        return len(self._shape)
 
     @property
     def dtype(self):
@@ -116,12 +179,40 @@ class ChunkGrid:
         return self._chunks
 
     def __getitem__(self, key):
-        normalized_key = tuple(normalize_slice(k, s) for k, s in zip(key, self.shape))
-        return self._data[normalized_key]
+        chunk_indices = tuple(
+            slice_to_chunk_index(k, o) for k, o in zip(key, self._offsets)
+        )
+        data = self._data[chunk_indices]
+        if data is None:
+            chunk_shape = tuple(
+                chunksizes[index]
+                for index, chunksizes in zip(chunk_indices, self.chunks)
+            )
+            data = sparse.full(
+                chunk_shape, fill_value=self.fill_value, dtype=self.dtype
+            )
+
+        result = type(self)(
+            shape=data.shape,
+            dtype=data.dtype,
+            order=self.order,
+            fill_value=data.fill_value,
+            chunks=None,
+        )
+        result._data = data
+        return result
 
     def __setitem__(self, key, value):
-        normalized_key = tuple(normalize_slice(k, s) for k, s in zip(key, self.shape))
-        self._data[normalized_key] = value
+        if any(
+            s == slice(None) and size != total_size
+            for s, size, total_size in zip(key, value.shape, self.shape)
+        ):
+            raise ValueError("size mismatch")
+
+        chunk_indices = tuple(
+            slice_to_chunk_index(k, o) for k, o in zip(key, self._offsets)
+        )
+        self._data[chunk_indices] = value
 
     def get_value(self):
         chunk_sizes = [
@@ -130,10 +221,32 @@ class ChunkGrid:
         ]
         return chunk_sizes
 
+    def all_equal(self, other: Any, equal_nan: bool) -> bool:
+        if other.ndim != 0 and (
+            self.shape != other.shape
+            or self.dtype != other.dtype
+            or self.order != other.order
+            or self.fill_value != other.fill_value
+            or self.chunks != other.chunks
+        ):
+            return False
+
+        if other.ndim == 0:
+            to_compare = (
+                (c, other._data if isinstance(other, ChunkGrid) else other)
+                for c in self._data.ravel().tolist()
+            )
+        else:
+            to_compare = zip(self._data.ravel().tolist(), other._data.ravel().tolist())
+
+        return all(sparse_equal(a, b, equal_nan=equal_nan) for a, b in to_compare)
+
 
 @register_ndbuffer
 class SparseNDBuffer(NDBuffer):
     def __init__(self, chunk_grid) -> None:
+        if chunk_grid is None:
+            raise ValueError("chunk grid is `None`")
         self._data = chunk_grid
 
     @classmethod
@@ -144,10 +257,15 @@ class SparseNDBuffer(NDBuffer):
         dtype: npt.DTypeLike,
         order: Literal["C", "F"] = "C",
         fill_value: Any | None = None,
+        chunks=None,
     ) -> Self:
         return cls(
             ChunkGrid(
-                shape=tuple(shape), dtype=dtype, order=order, fill_value=fill_value
+                shape=tuple(shape),
+                dtype=dtype,
+                order=order,
+                fill_value=fill_value,
+                chunks=chunks,
             )
         )
 
@@ -156,12 +274,13 @@ class SparseNDBuffer(NDBuffer):
         return cls.from_ndarray_like(array_like)
 
     @classmethod
-    def from_ndarray_like(cls, ndarray_like) -> Self:
+    def from_ndarray_like(cls, ndarray_like, chunks=None) -> Self:
         buffer = cls.create(
             shape=ndarray_like.shape,
             dtype=ndarray_like.dtype,
             order="C",
             fill_value=ndarray_like.fill_value,
+            chunks=chunks or getattr(ndarray_like, "chunks", None),
         )
         buffer[(slice(None),) * ndarray_like.ndim] = ndarray_like
 
@@ -205,37 +324,7 @@ class SparseNDBuffer(NDBuffer):
             # Handle None fill_value for Zarr V2
             return False
 
-        equal_nan = (
-            equal_nan
-            if self._data.dtype.kind not in ("U", "S", "T", "O", "V")
-            else False
-        )
-        if other.ndim == 0:
-            return np.array_equal(
-                self._data.fill_value, other, equal_nan=equal_nan
-            ) and (
-                self._data.nnz == 0
-                or np.equal(
-                    self._data.data,
-                    np.broadcast_to(other, self._data.data.shape),
-                    equal_nan,
-                )
-            )
-
-        # use array_equal to obtain equal_nan=True functionality
-        # Since fill-value is a scalar, isn't there a faster path than allocating a new array for fill value
-        # every single time we have to write data?
-        _data, other = sparse.broadcast_arrays(self._data, other)
-
-        return sparse.equal(
-            self._data,
-            other,
-            equal_nan=(
-                equal_nan
-                if self._data.dtype.kind not in ("U", "S", "T", "O", "V")
-                else False
-            ),
-        )
+        return self._data.all_equal(other, equal_nan=equal_nan)
 
 
 buffer_prototype = BufferPrototype(buffer=Buffer, nd_buffer=SparseNDBuffer)
