e293a3e27b62a27439c965849bff95f408318b26
Justus Magin <keewis@posteo.de>
2025-08-07 15:54:56 +0200

    implement encoding sparse arrays

--- zarr_sparse/codec.py
+++ zarr_sparse/codec.py
@@ -2,42 +2,110 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
 from zarr.abc.codec import ArrayBytesCodec
-from zarr.core.buffer import Buffer, NDBuffer
+from zarr.buffer.cpu import numpy_buffer_prototype
+from zarr.codecs import BytesCodec, ZstdCodec
+from zarr.codecs.sharding import MAX_UINT_64
+from zarr.core.array_spec import ArrayConfig, ArraySpec
+from zarr.core.buffer import Buffer, NDBuffer, default_buffer_prototype
 from zarr.core.common import JSON
-from zarr.registry import register_codec
+from zarr.core.dtype.npy.int import UInt64
+from zarr.registry import get_pipeline_class, register_codec
+
+from zarr_sparse.sparse import extract_arrays
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
-
     from zarr.core.array_spec import ArraySpec
 
 
+async def encode_array(array, chunk_spec, codecs):
+    pipeline = get_pipeline_class().from_codecs(codecs)
+
+    ndbuffer = numpy_buffer_prototype().nd_buffer.from_numpy_array(array)
+    array_bytes = next(iter(await pipeline.encode([(ndbuffer, chunk_spec)])))
+    metadata = {
+        "size": array.size,
+        "dtype": array.dtype,
+        "order": "C",
+    }
+
+    return metadata, array_bytes
+
+
+async def encode_table(metadata, codecs):
+    sizes = np.array([m["size"] for m in metadata], dtype="uint64")
+    chunk_spec = ArraySpec(
+        shape=sizes.shape,
+        dtype=UInt64(endianness="little"),
+        fill_value=MAX_UINT_64,
+        config=ArrayConfig(order="C", write_empty_chunks=False),
+        prototype=default_buffer_prototype(),
+    )
+    ndbuffer = numpy_buffer_prototype().nd_buffer.from_numpy_array(sizes)
+
+    pipeline = get_pipeline_class().from_codecs(codecs)
+    table_bytes = next(iter(await pipeline.encode([(ndbuffer, chunk_spec)])))
+
+    return table_bytes
+
+
 class SparseArrayCodec(ArrayBytesCodec):
+    def __init__(self):
+        self.array_codecs = (BytesCodec(), ZstdCodec())
+        self.table_codecs = (BytesCodec(),)
+
     def to_dict(self) -> dict[str, JSON]:
         return {"name": "sparse"}
 
     async def _decode_single(
         self, chunk_data: NDBuffer, chunk_spec: ArraySpec
     ) -> Buffer | None:
-        raise NotImplementedError
+        raise NotImplementedError(f"chunk data: {chunk_data}, chunk spec: {chunk_spec}")
 
-    async def decode(
-        self,
-        chunks_and_specs: Iterable[tuple[Buffer | None, ArraySpec]],
-    ) -> Iterable[NDBuffer | None]:
-        raise NotImplementedError
+    # async def decode(
+    #     self,
+    #     chunks_and_specs: Iterable[tuple[Buffer | None, ArraySpec]],
+    # ) -> Iterable[NDBuffer | None]:
+    #     print(list(chunks_and_specs))
+    #     raise NotImplementedError
 
     async def _encode_single(
         self, chunk_array: NDBuffer, chunk_spec: ArraySpec
     ) -> Buffer | None:
-        raise NotImplementedError
+        data = chunk_array._data.get_chunk()
+        if data.nnz == 0 and not chunk_spec.config.write_empty_chunks:
+            return None
 
-    async def encode(
-        self,
-        chunks_and_specs: Iterable[tuple[NDBuffer | None, ArraySpec]],
-    ) -> Iterable[Buffer | None]:
-        raise NotImplementedError
+        metadata, arrays = extract_arrays(data)
+
+        prototype = numpy_buffer_prototype()
+
+        encoded = []
+        array_metadata = []
+        for array in arrays:
+            spec = ArraySpec(
+                shape=array.shape,
+                dtype=array.dtype,
+                fill_value=0,
+                config=chunk_spec.config,
+                prototype=prototype,
+            )
+            metadata, encoded_array = await encode_array(array, spec, self.array_codecs)
+            encoded.append(encoded_array)
+            array_metadata.append(metadata)
+
+        merged_buffer = sum(encoded[1:], start=encoded[0])
+
+        table_buffer = await encode_table(array_metadata, self.table_codecs)
+
+        return table_buffer + merged_buffer
+
+    # async def encode(
+    #     self,
+    #     chunks_and_specs: Iterable[tuple[NDBuffer | None, ArraySpec]],
+    # ) -> Iterable[Buffer | None]:
+    #     raise NotImplementedError
 
 
 register_codec("sparse", SparseArrayCodec)
