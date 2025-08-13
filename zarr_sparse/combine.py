df3cec160f49b9448020acc8f54d5e252df99f9e
Justus Magin <keewis@posteo.de>
2025-08-13 18:33:58 +0200

    add tests for `combine_nd` and its related functions

--- zarr_sparse/combine.py
+++ zarr_sparse/combine.py
@@ -44,25 +44,3 @@ def combine_nd(parts):
         }
 
     return next(iter(tiles.values()))
-
-
-def expand_chunks(chunks, shape):
-    def _expand(chunkspec, size):
-        if chunkspec == -1:
-            return (size,)
-        elif isinstance(chunkspec, int):
-            n_full_chunks, remainder = divmod(size, chunkspec)
-            chunks = (chunkspec,) * n_full_chunks
-
-            if remainder > 0:
-                chunks += (remainder,)
-
-            return chunks
-
-        chunkspec = tuple(chunkspec)
-        if sum(chunkspec) != size:
-            raise ValueError(f"chunks don't add up to the full size: {chunkspec}")
-
-        return chunkspec
-
-    return tuple(_expand(chunkspec, size) for chunkspec, size in zip(chunks, shape))
