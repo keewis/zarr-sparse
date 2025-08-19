7f947ededfe480a5cae807091b94ac4ff5aa66b9
Justus Magin <keewis@posteo.de>
2025-08-19 11:10:18 +0200

    use `struct` to reimplement the metadata codec

---
+++ zarr_sparse/codec/metadata.py
@@ -0,0 +1,130 @@
+import struct
+from functools import partial
+
+
+def slice_next(offset, size):
+    return slice(offset, offset + size)
+
+
+def calculate_offset_size(offset, sizes):
+    _offset = offset
+    for size in sizes:
+        yield offset, size
+
+        offset += size
+
+
+def encode_ints(ints, format="L"):
+    length = len(ints)
+
+    format_str = f"<H{length}{format}"
+    return struct.pack(format_str, length, *ints)
+
+
+def decode_ints(bytes_, format="L"):
+    [length] = struct.unpack("<H", bytes_[:2])
+
+    format_ = struct.Struct(f"<{length}{format}")
+    nbytes = format_.size
+    return list(format_.unpack(bytes_[2 : nbytes + 2]))
+
+
+def encode_string(s):
+    return s.encode("utf-8")
+
+
+def decode_string(bytes_):
+    return bytes_.decode("utf-8")
+
+
+def encode_strings(strings, size_format="H"):
+    encoded = [encode_string(s) for s in strings]
+    nbytes_per_entry = [len(b) for b in encoded]
+
+    table = encode_ints(nbytes_per_entry, format=size_format)
+
+    return table + b"".join(encoded)
+
+
+def decode_strings(bytes_, size_format="H"):
+    nbytes_per_entry = decode_ints(bytes_, format=size_format)
+
+    format_str = f"<H{len(nbytes_per_entry)}{size_format}"
+    offset = struct.calcsize(format_str)
+    encoded = (
+        bytes_[slice_next(o, s)]
+        for o, s in calculate_offset_size(offset, nbytes_per_entry)
+    )
+
+    return [decode_string(b) for b in encoded]
+
+
+encoders = {
+    "sparse-kind": encode_string,
+    "order": partial(encode_strings, size_format="B"),
+    "nbytes": partial(encode_ints, format="L"),
+    "sizes": partial(encode_ints, format="L"),
+    "compressed_axes": partial(encode_ints, format="H"),
+}
+decoders = {
+    "sparse-kind": decode_string,
+    "order": partial(decode_strings, size_format="B"),
+    "nbytes": partial(decode_ints, format="L"),
+    "sizes": partial(decode_ints, format="L"),
+    "compressed_axes": partial(decode_ints, format="H"),
+}
+
+
+def _encode_metadata_table(metadata):
+    """encode the metadata table to bytes
+
+    The format is:
+    1. offset table (offset + size for each entry)
+    2. array of column names
+    3. data arrays
+
+    The new format is:
+    1. offset table (offset + n_entries for each entry)
+    2. list of column names
+    3. data values
+
+    Each array is encoded as
+    1. 2 bytes for length
+    2. array data bytes
+    """
+    metadata_ = dict(metadata)
+
+    # ignore the dtypes – these can be reconstructed from the array metadata
+    metadata_.pop("dtypes", None)
+
+    column_names = encode_strings(list(metadata_), size_format="B")
+    values = [encoders.get(name)(column) for name, column in metadata_.items()]
+
+    metadata_table_bytes = [column_names] + values
+    nbytes = [len(v) for v in metadata_table_bytes]
+
+    initial_offset = len(nbytes) * 2 + 2  # format = "H" takes 2 bytes
+    offsets, _ = zip(*calculate_offset_size(initial_offset, nbytes))
+
+    offset_bytes = encode_ints(offsets, format="H")
+
+    all_bytes = [offset_bytes] + metadata_table_bytes
+
+    return b"".join(all_bytes)
+
+
+def _decode_metadata_table(bytes_):
+    offsets = decode_ints(bytes_, format="H")
+    offsets_ = offsets + [len(bytes_)]
+    sizes = [upper - lower for lower, upper in zip(offsets_[:-1], offsets_[1:])]
+
+    column_name_bytes, *column_bytes = [
+        bytes_[slice_next(offset, size)] for offset, size in zip(offsets, sizes)
+    ]
+    column_names = decode_strings(column_name_bytes, size_format="B")
+
+    decoded = {
+        name: decoders.get(name)(encoded_column)
+        for name, encoded_column in zip(column_names, column_bytes, strict=False)
+    }
+    return decoded
---
+++ zarr_sparse/tests/test_metadata_codec.py
@@ -0,0 +1,113 @@
+import pytest
+
+from zarr_sparse.codec import metadata
+
+
+@pytest.mark.parametrize(
+    ["ints", "format", "expected"],
+    (
+        ([160, 1081], "H", b"\x02\x00\xa0\x00\x39\x04"),
+        (
+            [233, 233, 233],
+            "L",
+            b"\x03\x00\xe9\x00\x00\x00\xe9\x00\x00\x00\xe9\x00\x00\x00",
+        ),
+    ),
+)
+def test_encode_ints(ints, format, expected):
+    actual = metadata.encode_ints(ints, format=format)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ["bytes_", "format", "expected"],
+    (
+        (b"\x02\x00\xa0\x00\x39\x04", "H", [160, 1081]),
+        (
+            b"\x03\x00\xe9\x00\x00\x00\xe9\x00\x00\x00\xe9\x00\x00\x00",
+            "L",
+            [233, 233, 233],
+        ),
+    ),
+)
+def test_decode_ints(bytes_, format, expected):
+    actual = metadata.decode_ints(bytes_, format=format)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ["ints", "format"],
+    (
+        (list(range(20)), "H"),
+        (list(range(300000, 300020)), "L"),
+    ),
+)
+def test_ints_roundtrip(ints, format):
+    encoded = metadata.encode_ints(ints, format=format)
+
+    decoded = metadata.decode_ints(encoded, format=format)
+
+    assert decoded == ints
+
+
+@pytest.mark.parametrize(
+    ["strings", "format", "expected"],
+    (
+        (["a", "bc", "def"], "H", b"\x03\x00\x01\x00\x02\x00\x03\x00abcdef"),
+        (
+            ["abc", "d", "ef", "ghijkl"],
+            "H",
+            b"\x04\x00\x03\x00\x01\x00\x02\x00\x06\x00abcdefghijkl",
+        ),
+        (["C", "C"], "B", b"\x02\x00\x01\x01CC"),
+    ),
+)
+def test_encode_strings(strings, format, expected) -> None:
+    actual = metadata.encode_strings(strings, size_format=format)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ["bytes_", "format", "expected"],
+    (
+        (b"\x03\x00\x01\x00\x02\x00\x03\x00abcdef", "H", ["a", "bc", "def"]),
+        (
+            b"\x04\x00\x03\x00\x01\x00\x02\x00\x06\x00abcdefghijkl",
+            "H",
+            ["abc", "d", "ef", "ghijkl"],
+        ),
+        (b"\x02\x00\x01\x01CC", "B", ["C", "C"]),
+    ),
+)
+def test_decode_strings(bytes_, format, expected):
+    actual = metadata.decode_strings(bytes_, size_format=format)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "sparse_metadata",
+    (
+        {
+            "sparse-kind": "coo",
+            "nbytes": [10274, 1081, 742],
+            "sizes": [233, 233, 233],
+            "order": ["C", "C", "C"],
+        },
+        {
+            "sparse-kind": "gcxs",
+            "nbytes": [10274, 1081, 742],
+            "sizes": [233, 233, 35],
+            "order": ["C", "C", "C"],
+            "compressed_axes": [0],
+        },
+    ),
+)
+def test_metadata_roundtrip(sparse_metadata):
+    encoded = metadata._encode_metadata_table(sparse_metadata)
+    decoded = metadata._decode_metadata_table(encoded)
+
+    assert sparse_metadata == decoded
