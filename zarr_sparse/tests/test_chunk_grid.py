a814c76a0dea489dc9a98fb0c96b141e103efa47
Justus Magin <keewis@posteo.de>
2025-09-09 22:12:53 +0200

    check init and repr

--- zarr_sparse/tests/test_chunk_grid.py
+++ zarr_sparse/tests/test_chunk_grid.py
@@ -17,3 +17,30 @@ def test_readjust_chunk_keys(chunk_keys):
 
     min_values = tuple(min(v) for v in zip(*actual.keys()))
     assert min_values == (0,) * len(min_values)
+
+
+def test_init():
+    shape = (10, 10)
+    chunk_shape = (5, 5)
+    grid = chunk_grid.ChunkGrid(shape=shape, chunk_shape=chunk_shape)
+
+    assert grid.shape == shape
+    assert grid.chunk_shape == chunk_shape
+
+    assert grid.bounds == {}
+    assert grid.data == {}
+
+    assert grid.ndim == 2
+    assert grid.offsets == {}
+
+
+def test_repr():
+    shape = (10, 10)
+    chunk_shape = (5, 5)
+    grid = chunk_grid.ChunkGrid(shape=shape, chunk_shape=chunk_shape)
+
+    actual = repr(grid)
+
+    assert "ChunkGrid" in actual
+    assert f"shape={shape}" in actual
+    assert f"chunk_shape={chunk_shape}" in actual
