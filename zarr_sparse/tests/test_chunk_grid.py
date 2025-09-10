f2034705abeb591a2e717d29f7d972a6d03084b4
Justus Magin <keewis@posteo.de>
2025-09-10 10:59:19 +0200

    check getitem

--- zarr_sparse/tests/test_chunk_grid.py
+++ zarr_sparse/tests/test_chunk_grid.py
@@ -1,7 +1,18 @@
+import math
+
 import numpy as np
 import pytest
 
 from zarr_sparse import chunk_grid
+
+
+def gen_1d_bounds(start, stop, chunks):
+    size = stop - start
+    n_chunks = math.ceil(size / chunks)
+    return {
+        (idx,): (range(start + idx * chunks, start + (idx + 1) * chunks, 1),)
+        for idx in range(n_chunks)
+    }
 
 
 @pytest.mark.parametrize(
@@ -62,9 +73,7 @@ def test_select_keys(keys):
 
     indexers = [(slice(index * 2, (index + 1) * 2, 1),) for index, in keys]
 
-    assert len(actual.bounds) == len(keys)
-    assert list(actual.bounds.keys()) == [(0,), (1,)]
-    assert list(actual.bounds.values()) == [(range(*s.indices(10)),) for s, in indexers]
+    assert actual.bounds == gen_1d_bounds(0, 4, 2)
     assert [d.tolist() for d in actual.data.values()] == [
         data[s].tolist() for s in indexers
     ]
@@ -106,3 +115,24 @@ def test_setitem_explicit():
 
     grid[0:5, 0:4] = data[0:5, 0:4]
     assert grid.bounds == {(0, 0): (range(0, 5, 1), range(0, 4, 1))}
+
+
+@pytest.mark.parametrize(
+    ["indexer", "expected_bounds"],
+    (
+        (slice(0, 5), gen_1d_bounds(0, 6, 2)),
+        (slice(1, 4), gen_1d_bounds(0, 4, 2)),
+        (slice(4, 6), gen_1d_bounds(0, 2, 2)),
+        (slice(5, 7), gen_1d_bounds(0, 4, 2)),
+    ),
+)
+def test_getitem(indexer, expected_bounds):
+    grid = chunk_grid.ChunkGrid(shape=(7,), chunk_shape=(2,))
+    data = np.arange(7)
+
+    grid.bounds = {(idx,): (range(idx * 2, (idx + 1) * 2, 1),) for idx in range(4)}
+    grid.data = {(idx,): data[idx * 2 : (idx + 1) * 2] for idx in range(4)}
+
+    actual = grid[(indexer,)]
+
+    assert actual.bounds == expected_bounds
